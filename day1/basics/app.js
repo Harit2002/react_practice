5e1bea6e0e86e904f7630dbd4cd5379ad5b7557b
harit <haritkhushwas@gmail.com>
2024-01-04 15:56:23 +0530

    initial comment

---
+++ day1/basics/app.js
@@ -0,0 +1,19 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "hello world from react"
+);
+
+const react_elements = [
+  React.createElement("div", { id: "child" }),
+  [
+    React.createElement("h1", {}, "This is heading 1."),
+    React.createElement("h2", {}, "This is heading 2."),
+  ],
+];
+
+const parent = React.createElement("div", { id: "parent" }, react_elements);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
